4224af2760609878019f403cf3ee44bb96ad3b21
Wes Higbee <wes.mcclure@gmail.com>
2023-12-01 23:42:24 -0600

    nixfmt icat.nix => fixes in place

--- hello/icat.nix
+++ hello/icat.nix
@@ -1,8 +1,5 @@
 # icat.nix
-{ lib
-, stdenv
-, fetchFromGitHub
-}:
+{ lib, stdenv, fetchFromGitHub }:
 
 # 
 
@@ -15,10 +12,9 @@ stdenv.mkDerivation {
     rev = "v0.5";
     # nix-prefetch-url --unpack https://github.com/atextor/icat/archive/refs/tags/v0.5.zip
     #   FYI if not unpacked the shasum will be different, we need shasum of unpacked source
-    sha256 = "0wyy2ksxp95vnh71ybj1bbmqd5ggp13x3mk37pzr99ljs9awy8ka"; 
+    sha256 = "0wyy2ksxp95vnh71ybj1bbmqd5ggp13x3mk37pzr99ljs9awy8ka";
   };
 }
-
 
 ## FYI I stumbled on nixfmt tool mentioned and it installs with nix-env -i nixfmt!
 #  then nixfmt icat.nix
